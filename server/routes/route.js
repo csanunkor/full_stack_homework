e7bb93c2a6fe9ebb8cf72c5135c340a8d8a1273f
csanunkor <csanunkor@gmail.com>
2023-12-23 20:32:03 -0800

    remove node_modules from server

--- server/routes/route.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { readFile, getFileTree } = require("../controller/controller.js");
-
-router.get("/readfile", readFile);
-router.get("/filetree", getFileTree);
-
-module.exports = router;
